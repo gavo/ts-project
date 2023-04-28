40a69d1289e2c848be876163191f5f2b2ecf4a47
Gavo <gvmgust@gmail.com>
2023-04-28 01:29:54 -0400

    Ejemplo de traspilación de codigo

---
+++ src/01-hello.ts
@@ -0,0 +1,2 @@
+const myName = 'Gustavo';
+console.log(myName);
---
+++ src/demo.ts
@@ -0,0 +1,42 @@
+//npx tsc .\src\demo.ts --outDir dist --target es6
+
+async () => {
+  const myCart = [];
+  const products = [];
+  const limit = 2;
+
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      method: 'GET',
+    });
+    const data = await rta.json();
+    products.concat(data);
+  }
+  function getTotal() {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+      total += products[i].prize;
+    }
+    return total;
+  }
+  function addProduct(index) {
+    if (getTotal() <= limit) {
+      myCart.push(products[index]);
+    }
+  }
+
+  await getProducts();
+  addProduct(1);
+  addProduct(2);
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina',
+    toString: function () {
+      return this.name + ' ' + this.lastName;
+    },
+  };
+  const rta = person.toString() + limit;
+  console.log(rta);
+};
