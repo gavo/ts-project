035f70584104976470372f2e26ba4515d2728c80
Gavo <gvmgust@gmail.com>
2023-05-03 01:10:00 -0400

    class 12: functions

---
+++ src/12-functions.ts
@@ -0,0 +1,35 @@
+(() => {
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+  function createProductToJson(
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size: Sizes
+  ) {
+    return {
+      title,
+      createdAt,
+      stock,
+      size,
+    };
+  }
+
+  const product1 = createProductToJson('P1', new Date(), 12, 'S');
+  console.log(product1);
+
+  const createProductToJsonV2 = (
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size?: Sizes
+  ) => {
+    return {
+      title,
+      createdAt,
+      stock,
+      size,
+    };
+  };
+  const product2 = createProductToJsonV2('product2', new Date(), 12);
+  console.log(product2);
+})();
